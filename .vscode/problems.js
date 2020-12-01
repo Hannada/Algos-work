0191f19aa9e094c4df1febab17be9401002e14a9
Hannada <dphanna@gmail.com>
2020-11-30 21:21:05 -0500

    Daily

--- .vscode/problems.js
+++ .vscode/problems.js
@@ -3,17 +3,17 @@
 // }
 
 function Perm () {
-    for (let x = 1; x <= this.length; x++ ) {}
+    // for (let x = 1; x <= this.length; x++ ) {}
 
-    for (let x =1; x <= this.length; x++) {}
+    // for (let x =1; x <= this.length; x++) {}
 
     return [] 
 }
 
-function digiRoot () {
+// function digiRoot () {
 
-}
+// }
 
-function caeser_cipher () {
+// function caeser_cipher () {
     
-}
+// }
