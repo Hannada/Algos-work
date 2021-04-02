ccbbe6a7a6306b9f71b7a3998b0075242b018d0a
Hannada <dphanna@gmail.com>
2021-04-01 21:13:47 -0400

    Prac 1

--- .vscode/problems.js
+++ .vscode/problems.js
@@ -1,16 +1,16 @@
 
 
 function Perm () {
-    for (let x = 1; x <= this.length; x++ ) {}
+    for (let x = 1; x <= this.length; x++ ) {} 
 
 
     return [] 
 }
 
-// function digiRoot () {
-//     for (let x = 1; x <= this.length; x++){}
-// }
+function digiRoot () {
+    for (let x = 1; x <= this.length; x++){}
+}
 
-// function caeser_cipher () {
+function caeser_cipher () {
     
-// }
+}
