044a5a37fc0045839d2c3c69ace0568bb7d2ada8
Hannada <dphanna@gmail.com>
2021-05-05 21:52:36 -0400

    Prac 3

--- .vscode/problems.js
+++ .vscode/problems.js
@@ -8,11 +8,11 @@ function Perm () {
 }
 
 function digiRoot () {
-    for (let x = 1; x <= this.length; x++){
-        if (x > 4){
-            return x 
-        }
-    }
+    // for (let x = 1; x <= this.length; x++){
+    //     if (x > 4){
+    //         return x 
+    //     }
+    // }
 }
 
 function caeser_cipher () {
